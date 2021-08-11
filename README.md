e793c7ff6c097f151b2f33b2063365a76725c667
James Hoctor <JEHoctor@protonmail.com>
2021-08-11 17:44:09 -0400

    initial data downloader

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # covid-visualizations
 This is an exploration of data available from the [Covid Act Now API](https://apidocs.covidactnow.org/).
+
+## API Key
+
+After aquiring an API key as described on the Covid Act Now page, store it in an environment variable as shown below.
+
+```bash
+$ export COVID_ACT_NOW_API_KEY=YOUR_KEY_HERE
+```
+
+Then, in the same bash session, run a python interpreter in the root directory of this repository.
+
+```python
+>>> import data
+>>> df = data.get_historic_states_data()
+```
---
+++ data.py
@@ -0,0 +1,24 @@
+import io
+import os
+
+import pandas as pd
+
+import requests
+
+
+COVID_ACT_NOW_API_KEY = os.getenv("COVID_ACT_NOW_API_KEY")
+
+HISTORIC_STATES_URL = f"https://api.covidactnow.org/v2/states.timeseries.csv?apiKey={COVID_ACT_NOW_API_KEY}"
+HISTORIC_COUNTIES_URL = f"https://api.covidactnow.org/v2/counties.timeseries.csv?apiKey={COVID_ACT_NOW_API_KEY}"
+HISTORIC_METROS_URL = f"https://api.covidactnow.org/v2/cbsas.timeseries.csv?apiKey={COVID_ACT_NOW_API_KEY}"
+
+def get_historic_states_data():
+    r = requests.get(HISTORIC_STATES_URL)
+    if r.status_code != 200:
+        raise RuntimeError(f"Download failure: received status {r.status_code}")
+
+    buf = io.StringIO(r.text)
+    try:
+        return pd.read_csv(buf)
+    finally:
+        buf.close()
